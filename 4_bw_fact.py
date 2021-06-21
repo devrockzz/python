6bf80a488509fd7cd20bd6bdf66427648efbff3b
mrittikachakraborty <mrittika.chakraborty27@gmail.com>
2021-06-21 17:46:44 +0530

    4_bw_fact

--- 4_bw_fact.py
+++ 4_bw_fact.py
@@ -1,5 +1,5 @@
 # factorial for a number
 def factorial(n):
     return 1 if n ==0 or n ==1 else n * factorial(n-1) #n! = n* (n-1)* (n-2)*...*1
-n = 6
+n = int (input("Enter a number: " ))
 print("The factorial for, ", n, "is:", factorial(n))
