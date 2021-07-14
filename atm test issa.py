055b352ac2b2ec27a39f8ab2906440b7431fd1cc
issa mishra <70427213+issamishra@users.noreply.github.com>
2021-07-14 05:08:51 -0700

    Update atm test issa.py

--- atm test issa.py
+++ atm test issa.py
@@ -1,8 +1,1 @@
-print("Welcome to Wayne Bank ATM.")  
-balance = 50000
-chance = 2
-restart ="Y", "y"
-while chance >=0:
-    pin = int(input("Please enter 4-digit PIN: "))
-    if pin ==(1111):       
-            
+
