21375b6aeef9d3be6d8a69a085153abc6b8afc2d
JasmineSatapathy <jsatapathy2000@gmail.com>
2021-07-19 18:47:38 +0530

    Inheritance example by jas

---
+++ Dojo_activity/Hierarchical_Inheritance_Jas/Hierarchical_Inheritance_Parent_Jas.py
@@ -0,0 +1,17 @@
+print("Hello, welcome to my restaurant !!\n")
+
+#Base Class
+
+class Food:
+      def food_ingredients(self):
+          self.x = "Spices"
+          self.y = "Salt"
+          self.z = "Sauce"
+          self.a = "Flour"
+          self.b = "Eggs"
+          self.c = "Vegetables" 
+          self.d = "Meat"
+          self.e = "Water"
+          self.f = "Cooking oil"
+          print("Food Menu:")
+
